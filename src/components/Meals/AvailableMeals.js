63277c867638f0b346d2a47d30592473fd9a22cb
abaker42 <anthony.baker390@gmail.com>
2022-09-16 00:25:29 -0400

    Forms added

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,48 +1,56 @@
 import React from 'react';
 import Card from '../UI/Card';
 import classes from './AvailableMeals.module.css';
+import MealItem from './MealItem/MealItem';
 
 const DUMMY_MEALS = [
 	{
 		id: 'm1',
 		name: 'Sushi',
-		description: 'Raw fish with avacado and cucumber',
-		price: 22.99,
+		descr: 'Raw fish with avacado and cucumber',
+		price: 22.7987,
 	},
 	{
 		id: 'm2',
 		name: 'Burger',
-		description: 'American classic with all the fixins',
+		descr: 'American classic with all the fixins',
 		price: 12.99,
 	},
 	{
-		id: 'm2',
+		id: 'm6',
 		name: 'Steak',
-		description: 'Try this prime cut with our special steak sauce',
-		price: 29.99,
+		descr: 'Try this prime cut with our special steak sauce',
+		price: 29.288,
 	},
 	{
 		id: 'm3',
 		name: 'Shrimp',
-		description: '8 Collossal prawns served with lemon butter',
-		price: 24.99,
+		descr: '8 Collossal prawns served with lemon butter',
+		price: 24.5,
 	},
 	{
 		id: 'm4',
 		name: 'Pasta',
-		description: 'You choose your protein with white or red sauce',
-		price: 18.99,
+		descr: 'You choose your protein with white or red sauce',
+		price: 18.0,
 	},
 	{
 		id: 'm5',
 		name: 'Salad',
-		description: 'Homegrown vegetables with your choice of dressing',
+		descr: 'Homegrown vegetables with your choice of dressing',
 		price: 14.99,
 	},
 ];
 
 const AvailableMeals = () => {
-	const mealsList = DUMMY_MEALS.map((meal) => <li>{meal.name}</li>);
+	const mealsList = DUMMY_MEALS.map((meal) => (
+		<MealItem
+			key={meal.id}
+			name={meal.name}
+			descr={meal.descr}
+			price={meal.price}
+		/>
+	));
 	return (
 		<section className={classes.meals}>
 			<Card>
